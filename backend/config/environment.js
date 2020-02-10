3b4b208cc88d10fd6e91719a4e617ce0d48eeeaf
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-02-10 17:16:24 +0000

    environment.js

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -2,7 +2,7 @@
 const port = process.env.port || 8000
 const dbURIPrefix = 'mongodb://localhost/'
 const dbName = 'disney-villains' //animal-kingdom
-const dbURI = `${dbURIPrefix}${dbName}`
+const dbURI = process.env.MONGODB_URI || `${dbURIPrefix}${dbName}`
 const secret = 'This is nick\'s really secret string that he says nobody is going to be able to guess'
 
 module.exports = {
